ddc56ae5ee5d1de4bccf6ea41feafe289c9232e4
Rithvik89 <innovation6178@gmail.com>
2023-02-05 18:42:07 +0530

    altered user schema

--- entities/users.go
+++ entities/users.go
@@ -1,10 +1,9 @@
 package entities
 
-import "time"
-
 type Users struct {
-	Id       uint `gorm:"primary_key;auto_increment"`
-	MailId   string
-	Password string
-	CreateAt time.Time
+	Id               uint   `gorm:"primary_key;auto_increment"`
+	MailId           string `gorm:"not null"`
+	Password         string
+	IdentityProvider string
+	SubjectId        string
 }
